b1fddbbf931021e1daf8df34ccf13919cdf7970b
LilTOJustice <muianick4@gmail.com>
2021-04-28 23:31:05 -0700

    Added font scaling, default is 2 height divided by width

--- viewer.cc
+++ viewer.cc
@@ -5,10 +5,13 @@
 #include <vector>
 using namespace std;
 
+//VERY IMPORTANT: This number should be your terminal font height divided by width
+#define height_to_width 2
 //how much to zoom in when using w/a
 #define zoom_scale 0.1
 //how many pixels to move by when using arrow keys (is also scaled by screen size and zoom, so its not really one pixel per step)
 #define step_size 3
+
 
 int main() {
     int rows,cols;
@@ -24,18 +27,20 @@ int main() {
     set_raw_mode(true);
     while (true) {
         if (redraw) {
+            show_cursor(0);
             int shh = system("clear");
             shh = shh;
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
                     long double cal_row = ((rows/2 - 1) - i)/zoom + cur_y, cal_col = (j - (cols/2 - 1))/zoom + cur_x;
-                    complex<long double> c(cal_col/(cols/2), cal_row/(rows/2));
+                    complex<long double> c(cal_col/(cols/2), cal_row/(rows/2)/height_to_width);
                     cout << (fractal(c,fractals.at(fractal_index)) ? in_set : out_set);
                 }
                 cout << '\n';
             }
             cout << "Iterations: " << iterations << ", Zoom: " << zoom << ", Position: " <<  '(' << cur_x << ',' << cur_y << ")\n";
             redraw = false;
+            show_cursor(1);
         }
         int ch = quick_read();
         redraw = true;
